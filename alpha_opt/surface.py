edc7c0f3852d6d2da8978fb6fd8d5cfedb60d14e
Matt Landreman <mattland@umd.edu>
2025-09-13 14:17:01 -0400

    Scaling for surface dofs is now optional

--- alpha_opt/surface.py
+++ alpha_opt/surface.py
@@ -12,6 +12,7 @@ def init_optimizable_surface(
     major_radius,
     minor_radius,
     elongation=2.0,
+    scale=True,
     exponential_spectral_scaling_alpha=1.0,
 ):
     pre_surface = SurfaceRZFourier(
@@ -44,23 +45,28 @@ def init_optimizable_surface(
     # vmec._should_save_outputs = True  # If you want wout files to be generated.
 
     # Compute x_scale for the dofs.
-    # See ~/work24/20240415-01 Generating random stellarator boundary shapes.docx
-    # exponential_spectral_scaling_alpha = 1.0
-    ms = []
-    ns = []
-    for m in range(surface.mmin, surface.mmax + 1):
-        for n in range(surface.nmin, surface.nmax + 1):
-            if n == 0 and m in [0, 1]:
-                continue
-            ms.append(m)
-            ns.append(n)
+    if scale:
+        # See ~/work24/20240415-01 Generating random stellarator boundary shapes.docx
+        # exponential_spectral_scaling_alpha = 1.0
+        ms = []
+        ns = []
+        for m in range(surface.mmin, surface.mmax + 1):
+            for n in range(surface.nmin, surface.nmax + 1):
+                if n == 0 and m in [0, 1]:
+                    continue
+                ms.append(m)
+                ns.append(n)
 
-    ms = np.array(ms)
-    ns = np.array(ns)
-    dof_names_should_be = [f"Delta({m},{n})" for m, n in zip(ms, ns)]
-    assert surface.local_dof_names == dof_names_should_be, f"Expected {dof_names_should_be}, got {surface.local_dof_names}"
-    x_scale = np.exp(-exponential_spectral_scaling_alpha * np.sqrt((ms - 1)**2 + ns**2))
+        ms = np.array(ms)
+        ns = np.array(ns)
+        dof_names_should_be = [f"Delta({m},{n})" for m, n in zip(ms, ns)]
+        assert surface.local_dof_names == dof_names_should_be, f"Expected {dof_names_should_be}, got {surface.local_dof_names}"
+        x_scale = np.exp(-exponential_spectral_scaling_alpha * np.sqrt((ms - 1)**2 + ns**2))
+    else:
+        x_scale = np.ones_like(surface.x)
+        
     print("x_scale:", x_scale)
+
 
     x0 = surface.x / x_scale
 
