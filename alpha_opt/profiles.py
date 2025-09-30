0a64f0991d524a00c468d2041e13cc115cc9a9b5
Matt Landreman <mattland@umd.edu>
2025-09-29 20:15:39 -0400

    Added DT reaction rate

--- alpha_opt/profiles.py
+++ alpha_opt/profiles.py
@@ -1,5 +1,19 @@
+import numpy as np
+
 def n_m3_func(s, n0=3e20):
     return n0 * (1 - 2 * s**4 + 1.2 * s**6)
 
 def T_keV_func(s, T0=15):
     return T0 * (1 - 2 * s + 2 * s**2 - s**3)
+
+def DT_reaction_rate(ni, Ti_keV):
+    """DT fusion reaction rate from NRL plasma formulary, page 45 (2023 edition).
+    """
+    return ni * ni * (3.68e-12) * (Ti_keV**(-2.0/3.0) * np.exp(-19.94 * (Ti_keV**(-1.0/3.0))))
+
+def relative_DT_reaction_rate_for_our_profiles(s):
+    ni = n_m3_func(s) / 2
+    ni0 = n_m3_func(0) / 2
+    Ti_keV = T_keV_func(s)
+    Ti0_keV = T_keV_func(0)
+    return DT_reaction_rate(ni, Ti_keV) / DT_reaction_rate(ni0, Ti0_keV)
--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 from scipy.constants import elementary_charge
 import vmecpp
 from alpha_opt import DATA_DIR
-from alpha_opt.profiles import n_m3_func, T_keV_func
+from alpha_opt.profiles import n_m3_func, T_keV_func, DT_reaction_rate
 
 def test_pressure_profile_matches_vmec():
+    """Ensure that the profiles in python match the pressure profile in VMEC."""
     filename = os.path.join(DATA_DIR, "input.finite_beta")
     vmec_input = vmecpp.VmecInput.from_file(filename)
     vmec_output = vmecpp.run(vmec_input)
@@ -28,3 +29,22 @@ def test_pressure_profile_matches_vmec():
     # plt.show()
 
     np.testing.assert_allclose(pressure_vmec, pressure_should_be, rtol=2e-13)
+
+def test_reaction_rate():
+    """Compare the DT reaction rate to the function coded up by Michael C."""
+
+    s = np.linspace(0, 1, 100)[:-1]
+    def s_density(s):
+        return (
+            ((1 - s**5) ** 2)
+            * ((1 - s) ** (-2 / 3))
+            * np.exp(-19.94 * (12 * (1 - s)) ** (-1 / 3))
+        )
+    reaction_rate_Michael = s_density(s)
+
+    ni = 1 - s**5
+    Ti_keV = 12 * (1 - s)
+    reaction_rate_this_code = DT_reaction_rate(ni, Ti_keV)
+    # Don't worry about the constant in front, just make sure the ratio is constant
+    ratio = reaction_rate_Michael / reaction_rate_this_code
+    np.testing.assert_allclose(ratio, ratio[0], rtol=1e-14)
