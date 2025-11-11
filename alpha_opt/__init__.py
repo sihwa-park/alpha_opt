d97690595817ba70c832f49a1afae31fe0075bef
Matt Landreman <mattland@umd.edu>
2025-11-11 13:49:28 -0500

    Added constants with ARIES-CS minor radius

--- alpha_opt/__init__.py
+++ alpha_opt/__init__.py
@@ -2,6 +2,7 @@ import os
 
 DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data"))
 
+from .constants import ARIES_CS_MINOR_RADIUS
 from .loss_times import compute_energy_loss_fraction, time_at_which_energy_loss_exceeds
 from .pca import PCASurface
 from .surface import init_optimizable_surface
