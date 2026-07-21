1e93d7a9ac9a7363c8781748cdfcba2bc0184a63
Sihwa Park <iamsihwapark@gmail.com>
2026-07-21 15:52:19 -0400

    Add notebook files for analysis

--- alpha_opt/__init__.py
+++ alpha_opt/__init__.py
@@ -3,10 +3,10 @@ import os
 DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data"))
 
 from .constants import ARIES_CS_MINOR_RADIUS
-from .tracing.loss_times import (
-    compute_energy_loss_fraction,
-    time_at_which_energy_loss_exceeds,
-    alpha_loss_objective_from_times
-)
+# from .tracing.loss_times import (
+#     compute_energy_loss_fraction,
+#     time_at_which_energy_loss_exceeds,
+#     alpha_loss_objective_from_times
+# )
 from .pca import SurfacePCAGarabedian, SurfacePCARealSpace, SurfaceWeightedPCA
 from .visualization import make_videos, make_best_frame
